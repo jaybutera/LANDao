74bc6c8c40f20a5ea060655a498b43a3c38edb58
Jay Butera <buterajay@gmail.com>
2018-05-31 18:38:47 -0500

    Data dump

--- test/test.js
+++ test/test.js
@@ -1,14 +1,21 @@
 const DCLtoken = artifacts.require('DCLtoken')
 const MANACrowdsale = artifacts.require('MANACrowdsale')
+const MANAToken = artifacts.require('MANAToken')
+const ERC20 = artifacts.require('ERC20')
 
 contract('Test', accounts => {
-   let token, crowdsale
+   let token, crowdsale, MANAtoken
+   let manaAddress = accounts[1]
 
    before( async () => {
       token = await DCLtoken.new()
-      crowdsale = await MANACrowdsale.new()
+      MANAtoken = await MANAToken.new()
+      crowdsale = await MANACrowdsale.new(1, manaAddress, ERC20(token.address), ERC20(MANAtoken.address))
    })
 
    context('Fuknutz', async () => {
+      it('test', async () => {
+         assert(1==1)
+      })
    })
 })
