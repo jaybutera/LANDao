becbb1970e1d109f591f5172b7ff97497571d825
Jay Butera <buterajay@gmail.com>
2018-06-02 17:21:57 -0500

    Add MANA token

---
+++ test/tokens/MANAToken.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.11;
+
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
+//import "./BurnableToken.sol";
+
+contract MANAToken is BurnableToken, PausableToken, MintableToken {
+
+    string public constant symbol = "MANA";
+
+    string public constant name = "Decentraland MANA";
+
+    uint8 public constant decimals = 18;
+
+    function burn(uint256 _value) whenNotPaused public {
+        super.burn(_value);
+    }
+}
+
